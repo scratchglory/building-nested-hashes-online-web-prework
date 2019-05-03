f0350db049ff9080e09fda09eb6a9c76ef23d047
Adella <adellakenn@gmail.com>
2019-05-03 12:36:33 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,13 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+     #your key/value pairs here!
+     :patriarch
+     :matriarch
+     :hero 
+     :her0_friends
+      
+    }, 
     :capulet => {
       #your key/value pairs here!
       }}
