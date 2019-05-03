602d9f4209946ae99c5f272d2e4f7dead6c98bd1
Adella <adellakenn@gmail.com>
2019-05-03 12:40:53 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,7 +10,12 @@ def second_challenge
     }, 
     :capulet => {
       #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      
+    }}
 
   
 end
